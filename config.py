5bbfe2e3ab9ec651314e176b16042d82ff4c1761
Simone Stefani <s.stefani95@gmail.com>
2018-04-28 08:56:01 +0200

    Remove ignored files

--- config.py
+++
@@ -1,5 +0,0 @@
-# Path to the Golang code of the experiment
-# --------------------------------------------------------------
-# You can find such path by moving into the Go experiment code
-# (use "cd") and printing the working directory with "pwd"
-EXPERIMENT_PATH = '/Users/simone/Code/Go/src/github.com/SimoneStefani/thesis-algorithms'
