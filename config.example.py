222c7d137a51963db30267523a34e0da67a01213
Simone Stefani <s.stefani95@gmail.com>
2018-04-28 08:55:17 +0200

    Use env path

--- config.example.py
+++ config.example.py
@@ -2,4 +2,4 @@
 # --------------------------------------------------------------
 # You can find such path by moving into the Go experiment code
 # (use "cd") and printing the working directory with "pwd"
-EXPERIMENT_PATH = '/Users/simone/Code/Go/src/github.com/SimoneStefani/thesis-algorithms'
+EXPERIMENT_PATH = '/absolute/path/to/go/code'
